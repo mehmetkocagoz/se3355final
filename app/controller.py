95cff9f8deb8b7db1fbf03dcacb305ba0d30f9f3
Mehmet Kocagöz <mehmetkocagz@outlook.com>
2023-12-25 05:53:35 +0300

    Add vehicle grouping by price in getAll() function

--- app/controller.py
+++ app/controller.py
@@ -2,6 +2,17 @@
 
 from app.vehicle import getAllVehicles
 
+# We will group vehicles by their price
+# Some additional grouping or business logic can be apply
 def getAll():
+    vehicle_list = getAllVehicles()
+    grouped_vehicles = {}
+    for vehicle in vehicle_list:
+        group_key = getattr(vehicle, 'DailyRentPrice')
 
-    return getAllVehicles()
+        if group_key not in grouped_vehicles:
+            grouped_vehicles[group_key] = []
+
+        grouped_vehicles[group_key].append(vehicle)
+    print(grouped_vehicles.keys())
+    return grouped_vehicles
