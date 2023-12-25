99260e6c1611ac3eeef658d7ef1697a2ae90b2d8
Mehmet Kocagöz <mehmetkocagz@outlook.com>
2023-12-25 04:36:47 +0300

    Refactor routes.py to render all vehicles in the database

--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,14 @@
-from flask import render_template,request
+from flask import render_template
 from app import app
+from app.controller import getAll
 
 
 @app.route('/login')
 def login():
     return render_template('login.html')
 
+# Render with all vehicles in the database
 @app.route('/')
 def home():
+    vehicle_data = getAll()
     return render_template('index.html')
