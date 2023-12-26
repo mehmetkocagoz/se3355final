0f40f19101c688e6963f29c766955aaa2e8376e3
mehmetkocagoz <mehmetkocagz@outlook.com>
2023-12-26 13:33:57 +0300

    import office controller and temporarily make insertion from an endpoint

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,12 @@
 from flask import render_template
 from app import app
-from app.controller import getAll
+from app.vehiclecontroller import getAll
+from app.officecontroller import insertDataToTable
 
 
 @app.route('/login')
 def login():
+    insertDataToTable()
     return render_template('login.html')
 
 # Render with all vehicles in the database
