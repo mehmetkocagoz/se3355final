26913bbfd3e94d03efddc705b9c17f00959dd10d
Mehmet Kocagöz <mehmetkocagz@outlook.com>
2023-12-26 11:49:12 +0300

    Add new route for renting vehicles

--- app/routes.py
+++ app/routes.py
@@ -10,5 +10,9 @@ def login():
 # Render with all vehicles in the database
 @app.route('/')
 def home():
+    return render_template('index.html')
+
+@app.route('/rent')
+def rent():
     vehicle_data = getAll()
-    return render_template('index.html',vehicle_list = vehicle_data)
+    return render_template('rent.html',vehicle_list = vehicle_data)
