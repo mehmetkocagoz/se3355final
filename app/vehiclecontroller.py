35c81c849c0b999ac026577fadc1cc6ead25c5bd
Mehmet Kocagöz <mehmetkocagz@outlook.com>
2023-12-27 00:12:06 +0300

    Add image URL and calculate discounted price for vehicles

--- app/vehiclecontroller.py
+++ app/vehiclecontroller.py
@@ -1,18 +1,42 @@
 # Controller will interact with model which is vehicle
-
 from app.vehicle import getAllVehicles
+import base64
 
-# We will group vehicles by their price
-# Some additional grouping or business logic can be apply
+
 def getAll():
     vehicle_list = getAllVehicles()
-    grouped_vehicles = {}
+    
+    vehicle_list_array = []
     for vehicle in vehicle_list:
-        group_key = getattr(vehicle, 'DailyRentPrice')
+        Image = vehicle.Image
+        base64_image = base64.b64encode(Image).decode('utf-8')
+        mime_type = 'image/jpeg'
+        image_url = f'data:{mime_type};base64,{base64_image}'
 
-        if group_key not in grouped_vehicles:
-            grouped_vehicles[group_key] = []
+        # Discount will be applied based on the type of the car.
+        if vehicle.CarType == 'EKONOMİK':
+            discountRate = 30
+        else:
+            discountRate = 35
 
-        grouped_vehicles[group_key].append(vehicle)
+        discount = (vehicle.DailyRentPrice/100)*discountRate
+        discountedPrice = vehicle.DailyRentPrice - discount
+        vehicle_dict ={
+            'CarType' : vehicle.CarType,
+            'Brand' : vehicle.Brand,
+            'Model' : vehicle.Model,
+            'Transmission' : vehicle.Transmission,
+            'FuelType' : vehicle.FuelType,
+            'DepositPrice' : vehicle.DepositPrice,
+            'DailyRentPrice' : vehicle.DailyRentPrice,
+            'DiscountedPrice' : discountedPrice,
+            'Discount' : discount,
+            'DefaultKM' : vehicle.DefaultKM,
+            'Image' : image_url
+        }
+        
+        vehicle_list_array.append(vehicle_dict)
+    
+    return vehicle_list_array
 
-    return grouped_vehicles
+    
