8423888f7333e279c87b7e5a76cfa10c2339ab1a
Mehmet Kocagöz <mehmetkocagz@outlook.com>
2024-01-02 19:41:41 +0300

    Add user controller functions for registration and login

---
+++ app/usercontroller.py
@@ -0,0 +1,18 @@
+from app.user import insertUserToDatabase,checkUser
+
+def checkUserPasswordForRegisteration(password,password_again):
+    if password != password_again:
+        return False
+    else:
+        return True
+
+# There is no business logic    
+def createNewUser(username,password,country,city):
+    insertUserToDatabase(username,password,country,city)
+
+# There is no business logic
+def checkUsernamePasswordForLogin(username,password):
+    if checkUser(username,password) == True:
+        return True
+    else:
+        return False
