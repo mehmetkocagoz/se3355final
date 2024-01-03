28a067fa58c9fef055b6dda125257f2516f3d17d
Mehmet Kocagöz <mehmetkocagz@outlook.com>
2024-01-03 15:33:14 +0300

    Add office-related functions to office.py

--- app/office.py
+++ app/office.py
@@ -114,3 +114,39 @@ INSERT INTO city(city_name,latitude,longtitude)
                    ''',(city,latitude,longtitude))
     connection.commit()
     connection.close()
+
+def list_of_offices(user_city):
+    connection = connect()
+    cursor = connection.cursor()
+
+    cursor.execute("SELECT * FROM office WHERE office_city = ?",(user_city,))
+
+    list_of_offices = cursor.fetchall()
+
+    connection.commit()
+    connection.close()
+    return list_of_offices
+
+def office_id_query(pickup_office):
+    connection = connect()
+    cursor = connection.cursor()
+
+    cursor.execute("SELECT office_id FROM office WHERE office_title = ?",(pickup_office,))
+
+    office_id = cursor.fetchone()
+
+    connection.commit()
+    connection.close()
+    return office_id
+
+def car_id_list_query(office_id):
+    connection = connect()
+    cursor = connection.cursor()
+    office_id_value = office_id[0]
+    cursor.execute("SELECT car_id FROM car_office_assignment WHERE office_id = ?",(office_id_value,))
+
+    car_id_list = cursor.fetchall()
+
+    connection.commit()
+    connection.close()
+    return car_id_list
