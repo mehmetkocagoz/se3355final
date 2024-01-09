75470e8a6c77609de470025c0bd153c61ec02424
Mehmet Kocagöz <mehmetkocagz@outlook.com>
2024-01-09 19:07:25 +0300

    Update OAuth2 credentials using environment variables

--- app/__init__.py
+++ app/__init__.py
@@ -2,12 +2,15 @@ import pathlib
 from flask import Flask
 from google_auth_oauthlib.flow import Flow
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 # Create a Flask application instance
 app = Flask(__name__,static_url_path='/app/static')
 
 app.config.update(
-    OAUTH2_CLIENT_ID= "947748259257-m3m4rbtd91aa14pqlsheo61qihrq2nm4.apps.googleusercontent.com",
-    OAUTH2_CLIENT_SECRET="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",
+    OAUTH2_CLIENT_ID= os.environ.get('OAUTH2_CLIENT_ID'),
+    OAUTH2_CLIENT_SECRET=os.environ.get('OAUTH2_CLIENT_SECRET'),
     OAUTH2_META_URL="https://accounts.google.com/.well-known/openid-configuration",
     FLASK_SECRET= "RandomString",
     FLASK_PORT= 8000
